d72e481f12f390b7472322207e0fb1cbc8e5909b
Lorenzo Giannetti <118483922+LoreG002@users.noreply.github.com>
2025-07-01 22:14:04 +0200

    Aggiunto modello Parking e sincronizzazione tabella

---
+++ src/database.ts
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const sequelize = new Sequelize(
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        dialect: "postgres",
+        logging: false,  //posso mettere true se voglio vedere le query in console
+
+    }
+);
+
+export const testConnection = async () =>{
+    try{
+        await sequelize.authenticate();
+        console.log("Connessione al database avvenuta!")
+    } catch (error){
+        console.error("Errore di connessione al database:", error)
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import { testConnection } from "./database";
+import { Parking } from "./models/parking_model";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT;
+
+testConnection();
+
+(async() => {
+    try {
+        await Parking.sync({alter: true});
+        console.log("Tabella Parking Sincronizzata")
+    } catch(error){
+        console.log("Errore nella sincro della tabella parking:", error);
+    }
+})();
 
 app.get("/", (req, res) => {
-  res.send("Ciao! Il progetto è pronto 🚀");
+  res.send("Il progetto è pronto");
 });
 
 app.listen(PORT, () => {
   console.log(`Server avviato sulla porta ${PORT}`);
 });
+
+
