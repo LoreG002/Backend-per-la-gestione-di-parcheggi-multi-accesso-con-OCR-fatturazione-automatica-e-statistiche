5b6f6f37711b5597813bfe57fed691ad29d8b77e
Lorenzo Giannetti <118483922+LoreG002@users.noreply.github.com>
2025-07-07 17:33:38 +0200

    Modifca state_routes
    Ho aggiunto anche una media di posti liberi sempre con data di inizio e di fine del calcolo della statistica e anche una media di transiti per tipo di veicolo

--- src/routes/stats_routes.ts
+++ src/routes/stats_routes.ts
@@ -8,14 +8,14 @@ import { authenticateJWT } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.get("/api/stats/fatturato", authenticateJWT, async(req,res) =>{ ///api/stats/fatturato?startDate=2024-01-01&endDate=2025-12-31
+router.get("/api/stats/fatturato", authenticateJWT, async(req,res) =>{ // /api/stats/fatturato?startDate=2024-01-01&endDate=2025-12-31
     try{
         const {startDate, endDate} = req.query;
 
         const start = new Date(startDate as string);
         const end = new Date(endDate as string);
 
-        console.log("data di inizio:", start, "e data di fine: ", end);
+        //console.log("data di inizio:", start, "e data di fine: ", end);
 
         if(!startDate || !endDate){
             res.status(400).json({ message: "Le date di inizio e di fine sono obbligatorie" });
@@ -65,6 +65,65 @@ router.get("/api/stats/fatturato", authenticateJWT, async(req,res) =>{ ///api/st
         console.error(error);
         res.status(500).json({message: "Errore nel calcolo del fatturato!"});
     }
+});
+
+router.get("/api/stats/parking-usage", authenticateJWT, async (req,res) =>{ // /api/stats/parking-usage?startDate=2024-01-01&endDate=2025-12-31
+    try{
+        const { startDate, endDate} = req.query;
+
+        const start = new Date(startDate as string);
+        const end = new Date(endDate as string);
+
+        if(!startDate || !endDate ) {
+            res.status(400).json({message: "Le date di inizio e di fine sono obbligatorie! "});
+            return;
+        }
+
+        const parkings = await Parking.findAll();
+        const stats = [];
+
+        for (const parking of parkings) {
+            const gates = await Gate.findAll({
+            where: { parkingId: parking.id },
+        });
+
+        const gateIds = gates.map(g => g.id);
+
+        const mediaOccupati = await Transit.count({
+            where: {
+            gateId: { [Op.in]: gateIds },
+            direction: "entrata",
+            timestamp: { [Op.between]: [start, end] },
+            },
+        });
+
+        const postiLiberiMedi = Math.max(0, parking.capacity - mediaOccupati);
+
+        const transitiPerVeicolo = await Transit.findAll({
+            attributes: [
+            "vehicleTypeId",
+            [fn("COUNT", col("id")), "count"]],
+            where: {
+            gateId: { [Op.in]: gateIds },
+            timestamp: { [Op.between]: [start, end] },
+            },
+            group: ["vehicleTypeId"],
+        });
+
+        stats.push({
+            parkingId: parking.id,
+            parkingName: parking.name,
+            postiLiberiMedi,
+            transitiPerVeicolo,
+        });
+        }
+
+        res.json(stats);
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ message: "Errore nel calcolo delle statistiche"});
+    }
+        
 });
 
 export default router;
