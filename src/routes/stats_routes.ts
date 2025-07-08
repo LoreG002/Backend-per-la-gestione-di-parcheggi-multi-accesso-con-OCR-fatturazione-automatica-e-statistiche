ad05420e36cad6ab45a39f7c4214cffcfc4e1c39
Lorenzo Giannetti <118483922+LoreG002@users.noreply.github.com>
2025-07-08 12:21:13 +0200

    Modifica ulteriore di stats routes
    Aggiunte statistiche per singolo parcheggio

--- src/routes/stats_routes.ts
+++ src/routes/stats_routes.ts
@@ -5,6 +5,7 @@ import { Gate } from "../models/gate_model";
 import { Parking } from "../models/parking_model";
 import { Op, fn, col } from "sequelize";
 import { authenticateJWT } from "../middlewares/auth.middleware";
+import { literal } from "sequelize";
 
 const router = Router();
 
@@ -124,6 +125,104 @@ router.get("/api/stats/parking-usage", authenticateJWT, async (req,res) =>{ // /
         res.status(500).json({ message: "Errore nel calcolo delle statistiche"});
     }
         
+});
+
+router.get("/api/stats/parking/:parkingId", authenticateJWT, async (req,res) => { // /api/stats/parking/2?startDate=2024-01-01&endDate=2024-12-31
+    try {
+        const { parkingId } = req.params;
+        const { startDate, endDate } = req.query;
+
+        const parkingIdNum = parseInt(req.params.parkingId, 10);
+        console.log("ID convertito:", parkingIdNum);
+
+
+        console.log("req.params:", req.params);
+
+
+        console.log("l'id del parcheggio è: ",parkingIdNum, "oppure", parkingId);
+
+        const start= new Date(startDate as string);
+        const end = new Date(endDate as string);
+
+        if (!startDate || !endDate) {
+            res.status(400).json({ message: "Le date di inizio e di fine sono obbligatorie!" });
+            return;
+        }
+        const parking = await Parking.findByPk(parkingIdNum);
+        
+        if (!parking) {
+            res.status(404).json({ message: "Parcheggio non trovato" });
+            return;
+        }
+
+        const gates = await Gate.findAll({ 
+            where: { parkingId: parkingIdNum } 
+        });
+        const gateIds = gates.map(g => g.id);
+
+          const transitiTotali = await Transit.count({
+            where: {
+                gateId: { [Op.in]: gateIds },
+                timestamp: { [Op.between]: [start, end] }
+            }
+        }); 
+
+        const transitiPerVeicolo = await Transit.findAll({
+            attributes: [
+                "vehicleTypeId",
+                [fn("COUNT", col("id")), "count"]
+            ],
+            where: {
+                gateId: { [Op.in]: gateIds },
+                timestamp: { [Op.between]: [start, end] }
+            },
+            group: ["vehicleTypeId"]
+        });
+
+        const transitiPerFascia = await Transit.findAll({
+        attributes: [
+            [literal(`EXTRACT(HOUR FROM "timestamp")`), "fasciaOraria"],
+            [fn("COUNT", col("id")), "count"]
+        ],
+
+        where: {
+            gateId: { [Op.in]: gateIds },
+            timestamp: { [Op.between]: [start, end] }
+        },
+        group: ["fasciaOraria"]
+        });
+
+        const transitiUscita = await Transit.findAll({
+        where: {
+            gateId: { [Op.in]: gateIds },
+            direction: "uscita",
+            timestamp: { [Op.between]: [start, end] }
+        }
+        });
+
+        const invoiceIds = transitiUscita.map(t => t.invoiceId).filter(id => id != null);
+        const invoices = await Invoice.findAll({
+        where: { id: { [Op.in]: invoiceIds } }
+        });
+
+        const fatturato = invoices.reduce(
+        (acc, inv) => acc + parseFloat(inv.amount.toString()),
+        0
+        );
+
+        res.json({
+        parkingId: parking.id,
+        parkingName: parking.name,
+        transitiTotali,
+        transitiPerVeicolo,
+        transitiPerFascia,
+        fatturato
+        });
+
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({message: "Errore nel calcolo delle statistiche del parcheggio "});
+    }
 });
 
 export default router;
