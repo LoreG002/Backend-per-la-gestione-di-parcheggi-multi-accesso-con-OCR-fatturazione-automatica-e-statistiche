c4c23345528d3bff2914a101354fac49180318c4
grannico <niccolo1310@gmail.com>
2025-07-14 21:51:38 +0200

    correzione invoice_routes

--- src/routes/invoice.routes.ts
+++ src/routes/invoice.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as InvoiceController from "../controllers/invoice.controller";
 import { authenticateJWT } from "../middlewares/auth.middleware";
+import { authorizeRoles } from "../middlewares/role.middleware";
 
 const router = Router();
 
@@ -8,6 +9,6 @@ router.get("/api/invoices", authenticateJWT, InvoiceController.getAllInvoices);
 
 router.get("/api/invoices/status", authenticateJWT, InvoiceController.getInvoiceStatus);
 
-router.post("/api/invoices/:id/pay", authenticateJWT, InvoiceController.payInvoice);
+router.post("/api/invoices/:id/pay", authenticateJWT, authorizeRoles("utente"), InvoiceController.payInvoice);
 
 export default router;
