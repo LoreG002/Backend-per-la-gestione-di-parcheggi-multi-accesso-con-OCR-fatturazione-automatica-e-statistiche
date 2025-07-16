09d04bfc1143732afa7a88be3ff96c80863175ab
Lorenzo Giannetti <118483922+LoreG002@users.noreply.github.com>
2025-07-16 15:32:25 +0200

    aggiunto pattern singleton su Database.ts

--- src/database.ts
+++ src/database.ts
@@ -3,24 +3,38 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      dialect: "postgres",
-      logging: false,  //posso mettere true se voglio vedere le query in console
+class Database {
+  private static instance: Sequelize;
 
+  private constructor() {} // Costruttore privato per impedire l'istanziazione
+
+  public static getInstance(): Sequelize {
+    if (!Database.instance) {
+      Database.instance = new Sequelize(
+        process.env.DB_NAME as string,
+        process.env.DB_USER as string,
+        process.env.DB_PASSWORD,
+        {
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+          dialect: "postgres",
+          logging: false // puoi mettere true per vedere le query
+        }
+      );
     }
-);
-
-export const testConnection = async () =>{
-  try {
-    await sequelize.authenticate();
-    console.log("Connessione al database avvenuta!")
-  } catch (error) {
-    console.error("Errore di connessione al database:", error)
+    return Database.instance;
   }
-};
+
+  public static async testConnection() {
+    try {
+      await Database.getInstance().authenticate();
+      console.log("Connessione al database avvenuta!");
+    } catch (error) {
+      console.error("Errore di connessione al database:", error);
+    }
+  }
+}
+
+// Export delle due funzioni
+export const sequelize = Database.getInstance();
+export const testConnection = Database.testConnection;
