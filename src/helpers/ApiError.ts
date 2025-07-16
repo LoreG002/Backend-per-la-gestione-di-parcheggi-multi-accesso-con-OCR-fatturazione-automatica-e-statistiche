90fc5eb95a5adafacaaba2d28c09ccf63bf40ab1
Lorenzo Giannetti <118483922+LoreG002@users.noreply.github.com>
2025-07-16 16:39:17 +0200

    Commit Finale

--- src/helpers/ApiError.ts
+++ src/helpers/ApiError.ts
@@ -1,9 +1,13 @@
 export class ApiError extends Error {
+
+  // Proprietà per memorizzare lo status HTTP dell'errore
   public status: number;
 
+  // Costruttore che riceve lo status e il messaggio di errore
   constructor(status: number, message: string) {
     super(message);
     this.status = status;
-    Object.setPrototypeOf(this, ApiError.prototype); // fix instanceof
+    // Corregge la catena del prototipo per garantire che instanceof funzioni correttamente
+    Object.setPrototypeOf(this, ApiError.prototype);
   }
 }
